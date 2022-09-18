8ebbbe0c904a8757f5f669e7801f8a6ad368aaeb
Daniel Riezebos <daniel@marissen.net>
2022-09-18 14:07:09 +0200

    WIP: Began work on ConfigurationHistory

---
+++ app/code/Daniel/ConfigurationHistory/Controller/Adminhtml/Confighistory/Index.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Daniel\ConfigurationHistory\Controller\Adminhtml\Confighistory;
+
+use Magento\Backend\App\Action;
+use Magento\Backend\App\Action\Context;
+use Magento\Framework\App\Action\HttpGetActionInterface;
+use Magento\Framework\View\Result\PageFactory;
+
+class Index extends Action implements HttpGetActionInterface
+{
+    const MENU_ID = 'Daniel_ConfigurationHistory::configurationhistory';
+    protected PageFactory $pageFactory;
+
+    public function __construct(Context $context, PageFactory $pageFactory)
+    {
+        parent::__construct($context);
+        $this->pageFactory = $pageFactory;
+    }
+
+    public function execute()
+    {
+        $resultPage = $this->pageFactory->create();
+        $resultPage->setActiveMenu(static::MENU_ID);
+        $resultPage->getConfig()->getTitle()->prepend(__('History of Configuration'));
+
+        return $resultPage;
+    }
+}
---
+++ app/code/Daniel/ConfigurationHistory/Ui/Component/DataProvider.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Daniel\ConfigurationHistory\Ui\Component;
+
+use Magento\Framework\View\Element\UiComponent\DataProvider\DataProvider as Origin;
+
+class DataProvider extends Origin
+{
+
+}
