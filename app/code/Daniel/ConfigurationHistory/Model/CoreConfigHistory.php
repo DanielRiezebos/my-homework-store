e170c8d0a8fa00c5f261671f6778e27eaab932c8
Daniel Riezebos <daniel@marissen.net>
2022-09-25 13:52:19 +0200

    Added composer.json, module dependencies & slight formatting

--- app/code/Daniel/ConfigurationHistory/Model/CoreConfigHistory.php
+++ app/code/Daniel/ConfigurationHistory/Model/CoreConfigHistory.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace Daniel\ConfigurationHistory\Model;
 
 use Magento\Framework\Model\AbstractModel;
-use \Daniel\ConfigurationHistory\Model\ResourceModel\CoreConfigHistory as ResourceModel;
+use Daniel\ConfigurationHistory\Model\ResourceModel\CoreConfigHistory as ResourceModel;
 
 class CoreConfigHistory extends AbstractModel
 {
