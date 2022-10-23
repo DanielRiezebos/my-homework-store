916e37be643c82e51b2933075efd963622510c5a
Daniel Riezebos <daniel@marissen.net>
2022-10-23 12:47:33 +0200

    WIP: Initial commit Daniel_Duckonversions module

---
+++ app/code/Daniel/Duckonversions/Setup/InstallData.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Daniel\Duckonversions\Setup;
+
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Framework\Setup\InstallDataInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface;
+use Magento\Eav\Setup\EavSetupFactory;
+use Magento\Catalog\Model\Product;
+use Zend_Validate_Exception;
+
+class InstallData implements InstallDataInterface
+{
+    private EavSetupFactory $eavSetupFactory;
+    private const ATTRIBUTE_CODE = 'ducks';
+
+    public function __construct(EavSetupFactory $eavSetupFactory)
+    {
+        $this->eavSetupFactory = $eavSetupFactory;
+    }
+
+    /**
+     * @throws Zend_Validate_Exception
+     * @throws LocalizedException
+     */
+    public function install(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
+    {
+        $eavSetup = $this->eavSetupFactory->create();
+        $eavSetup->addAttribute(
+            Product::ENTITY,
+            self::ATTRIBUTE_CODE,
+            [
+                'type' => 'decimal',
+                'input' => 'text',
+                'group' => 'General',
+                'label' => 'Weight in Ducks',
+                'backend' => 'Magento\Catalog\Model\Product\Attribute\Backend\Weight',
+                'frontend' => '',
+                'source' => '',
+                'global' => ScopedAttributeInterface::SCOPE_GLOBAL,
+                'visible' => true,
+                'required' => false,
+                'is_used_in_grid' => true,
+                'is_visible_in_grid' => true,
+                'is_filterable_in_grid' => true,
+                'sort_order' => 42
+            ]
+        );
+    }
+}
---
+++ app/code/Daniel/Duckonversions/registration.php
@@ -0,0 +1,5 @@
+<?php
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Daniel_Duckonversions', __DIR__);
